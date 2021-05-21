8b24e26e927c75441f721cd42c71091256895013
Andrea Sforza <andrearobertosforza@gmail.com>
2021-05-21 11:59:20 +0200

    should be final

--- routes/file-upload.routes.js
+++ routes/file-upload.routes.js
@@ -2,15 +2,6 @@ const express = require('express');
 const fileUploadRoutes = express.Router();
 const fileUploader = require('../configs/cloudinary.config');
 
-// fileUploadRoutes.post('/uploadavatar', fileUploader.single('avatar'), (req, res, next) => {
-//     if(!req.file) {
-//         next(new Error('No file uploaded!'));
-//         return;
-//     }
-
-//     res.status(200).json({ secure_url: req.file.path })
-// })
-
 fileUploadRoutes.post('/upload', fileUploader.array('image', 10), (req, res, next) => {
     if(!req.files) {
         next(new Error('No file uploaded!'));
